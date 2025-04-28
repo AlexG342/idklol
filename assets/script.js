ef7d45a2d841c4b02b5f337e6c6c351c6f65b522
AlexG342 <90849307+AlexG342@users.noreply.github.com>
2025-04-28 04:38:33 +0000

    updated

--- assets/script.js
+++ assets/script.js
@@ -2,26 +2,4 @@ document.getElementById('menuToggle').addEventListener('click', () => {
     const menu = document.getElementById('dropdownMenu');
     menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
   });
-
-  window.addEventListener("DOMContentLoaded", () => {
-    const popup = document.getElementById("popupAd");
-    const closeBtn = document.getElementById("closePopup");
-  
-    // Show popup after 2 seconds
-    setTimeout(() => {
-      popup.style.display = "flex";
-    }, 2000);
-  
-    // Close when "X" is clicked
-    closeBtn.addEventListener("click", () => {
-      popup.style.display = "none";
-    });
-  
-    // Close if clicking outside the popup content
-    popup.addEventListener("click", (e) => {
-      if (e.target === popup) {
-        popup.style.display = "none";
-      }
-    });
-  });
   
